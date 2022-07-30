b77986fceb3f70e471bec89b0cbd1f372cc727de
Phuc <phuctv95@gmail.com>
2022-07-30 10:58:43 +0700

    init

---
+++ options.js
@@ -0,0 +1,36 @@
+const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+const selectedClassName = "current";
+const containerDiv = document.querySelector("#containerDiv");
+
+function handleButtonClick(event) {
+    const current = event.target.parentElement.querySelector(
+        `.${selectedClassName}`
+    );
+    if (current && current !== event.target) {
+        current.classList.remove(selectedClassName);
+    }
+
+    let color = event.target.dataset.color;
+    event.target.classList.add(selectedClassName);
+    chrome.storage.sync.set({ color });
+}
+
+function constructOptions(buttonColors) {
+    chrome.storage.sync.get("color", (data) => {
+        const currentColor = data.color;
+        for (let buttonColor of buttonColors) {
+            const button = document.createElement("button");
+            button.dataset.color = buttonColor;
+            button.style.backgroundColor = buttonColor;
+
+            if (buttonColor === currentColor) {
+                button.classList.add(selectedClassName);
+            }
+
+            button.addEventListener("click", handleButtonClick);
+            containerDiv.appendChild(button);
+        }
+    });
+}
+
+constructOptions(presetButtonColors);
---
+++ popup.js
@@ -0,0 +1,16 @@
+document
+    .querySelector('#changeColorBtn')
+    .addEventListener('click', async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: setPageBackgroundColor,
+        });
+    });
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get('color', ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
